87e968b813034da9a8abdb7f458558db9c8b4b7a
faheel <faheelkhan@hotmail.com>
2018-08-09 22:11:14 +0500

    jwt implemented

--- server-express/app.js
+++ server-express/app.js
@@ -31,26 +31,64 @@ var express = require('express');
 var jwt = require('jsonwebtoken');
 const app = express();
 
-app.get('/api', (req, res) => {
+app.get('/api', function(req, res) {
     res.json({
         text: 'my api'
     })
 });
 
-app.get('/api/protectedRoute', (req, res) => {
-    res.json({
-        text: 'protected Route'
-    });
-});
-
-app.post('/api/login', (req, res) => {
+app.post('/api/login', function(req, res) {
     // auth user
+    console.log(req.body);
+
     const user = { 'id': 3 };
-    const token = jwt.sign({ user }, 'my_secret_key');
+    const token = jwt.sign({ user: user.id }, 'secret_key_goes_here');
     res.json({
+        message: 'Authenticated! Use this token in the "Authorization" header',
         token: token
     });
 });
+
+app.get('/api/protected', ensureToken, function(req, res) {
+    console.log(req.token);
+    jwt.verify(req.token, 'secret_key_goes_here', function(err, data) {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                description: 'Protected information. Congrats!',
+                data: data
+            });
+        }
+    });
+});
+
+app.get('/api/getFaheel', ensureToken, function(req, res) {
+    console.log(req.token);
+    jwt.verify(req.token, 'secret_key_goes_here', function(err, data) {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                description: 'Protected information. Congrats!',
+                data: data
+            });
+        }
+    });
+});
+
+function ensureToken(req, res, next) {
+    const bearerHeader = req.headers["authorization"];
+    console.log(bearerHeader);
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
 app.listen(3000, function() {
     console.log('server on 3000');
 })
